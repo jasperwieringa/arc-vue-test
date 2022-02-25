228aa946b3a0c73b1d5e11d4f62ca556e4627626
Jasper.Wieringa <jasper.wieringa@arup.com>
2022-02-25 17:15:46 +0100

    Updated the Arc-Vue demo.

--- src/Index.vue
+++ src/Index.vue
@@ -9,9 +9,6 @@ import '@arc-web/components/dist/themes/index.css';
 import '@arc-web/components/dist/themes/light.css';
 import '@arc-web/components/dist/themes/dark.css';
 
-import { setBasePath } from '@arc-web/components/dist/utilities/base-path.js';
-setBasePath('/');
-
 export default {
   name: 'Index',
   components: {
--- src/components/App.vue
+++ src/components/App.vue
@@ -1,14 +1,12 @@
 <template>
   <arc-container :theme="theme">
-    <arc-navbar slot="nav" logo="./assets/arc-red.svg">
-      <arc-button type="tab">Tab One</arc-button>
-    </arc-navbar>
-    <arc-sidebar slot="side">
-      <div style="padding: var(--arc-spacing-small)">Sidebar content</div>
+    <arc-navbar slot="nav" logo="./assets/arc-red.svg"></arc-navbar>
+    <arc-sidebar slot="side" title="Sidebar title">
+      <div style="padding: var(--arc-spacing-medium)">Sidebar content</div>
     </arc-sidebar>
-    <div style="padding: var(--arc-spacing-small)">
-      Default content
-    </div>
+    <section aria-label="Demo section" style="padding: var(--arc-spacing-medium)">
+      Some content
+    </section>
   </arc-container>
 </template>
 
@@ -16,7 +14,6 @@
 import '@arc-web/components/dist/components/container/arc-container.js';
 import '@arc-web/components/dist/components/navbar/arc-navbar.js';
 import '@arc-web/components/dist/components/sidebar/arc-sidebar.js';
-import '@arc-web/components/dist/components/button/arc-button.js';
 
 export default {
   name: 'Arc',
